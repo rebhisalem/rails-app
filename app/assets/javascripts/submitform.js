5131a47871c04d835b33af35a7b6dc16686cd459
Salem Rabhi <rebhisalem@gmail.com>
2016-12-03 21:16:12 +0100

    modified

--- app/assets/javascripts/submitform.js
+++ app/assets/javascripts/submitform.js
@@ -3,6 +3,7 @@ jQuery(function($){
 	   $('#new_cand_submit').attr('disabled', true);
 
 	$('#candidat_nom').change(function(){
+		alert('hello');
 		if($('#candidat_nom').val().length >= 4 && $('#candidat_email').val().length >= 4) 
 			 $('#new_cand_submit').attr('disabled', false);    
 		else
