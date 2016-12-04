169856c584440d7b0e297a35c52209689c8bb7aa
Salem Rabhi <rebhisalem@gmail.com>
2016-12-04 11:00:44 +0100

    adding mailer after fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,6 @@ Rails.application.configure do
 	 :port                 => 587,
 	 :user_name            => 'infoshosting@gmail.com',
 	 :password             => '>Developper',
-	 :authentication       => "plain",
-	:enable_starttls_auto => true
+	 :authentication       => "plain"
 	}
 end
