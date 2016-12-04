307f6d1bc982194d25d526eafde63c3c05b8a1ca
Salem Rabhi <rebhisalem@gmail.com>
2016-12-04 12:45:14 +0100

    fix submit disabled

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,23 +16,3 @@
 //= require bootstrap-sprockets
 //= require submitform
 //= require_tree .
-
-$(function(){
-	if($('#candidat_nom').val().length < 4 || $('#candidat_email').val().length < 4) 
-	   $('#new_cand_submit').attr('disabled', true);
-
-	$('#candidat_nom').change(function(){
-		alert('hello');
-		if($('#candidat_nom').val().length >= 4 && $('#candidat_email').val().length >= 4) 
-			 $('#new_cand_submit').attr('disabled', false);    
-		else
-			 $('#new_cand_submit').attr('disabled', true);   
-	});
-	
-	$('#candidat_email').change(function(){
-		if($('#candidat_nom').val().length >= 4 && $('#candidat_email').val().length >= 4) 
-			 $('#new_cand_submit').attr('disabled', false);    
-		else
-			 $('#new_cand_submit').attr('disabled', true);   
-	});
-});
